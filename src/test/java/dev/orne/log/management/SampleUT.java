b2e81429877b5fdb05dced21c38733594a1a8bd5
(w) Iker Hernaez <ihernaez@users.noreply.github.com>
2025-12-08 21:35:55 +0100

    Updated license headers and disabled sample tests

--- src/test/java/dev/orne/log/management/SampleUT.java
+++ src/test/java/dev/orne/log/management/SampleUT.java
@@ -1,7 +1,30 @@
 package dev.orne.log.management;
 
+/*-
+ * #%L
+ * Orne Log Management
+ * %%
+ * Copyright (C) 2022 - 2025 Orne Developments
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * #L%
+ */
+
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -18,6 +41,7 @@ class SampleUT {
      * Sample test.
      */
     @Test
+    @Disabled
     void sampleUnitTest() {
         fail("Sample test");
     }
